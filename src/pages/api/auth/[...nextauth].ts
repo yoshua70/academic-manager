e96e60520a3e4399c16d10f56206ee7381f834b9
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-13 19:02:43 +0100

    feat: add custom credentials authentication
    Add custom credentials authentication using email and password.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,14 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+
+import * as argon2 from "argon2";
+import { User } from "@prisma/client";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -23,7 +27,33 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    CredentialsProvider({
+      name: "email",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "john@doe.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user: User | null = await prisma.user.findFirst({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        if (!user) return null;
+
+        console.log(credentials);
+        const isPasswordCorrect = await argon2.verify(
+          user.password,
+          credentials?.password!
+        );
+
+        console.log(isPasswordCorrect);
+        if (!isPasswordCorrect) return null;
+
+        return user;
+      },
+    }),
   ],
 };
 
