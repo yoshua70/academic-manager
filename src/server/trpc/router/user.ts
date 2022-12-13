1e459a11f5bb53ff600a791dbd4aba5d120a2f66
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-13 18:25:19 +0100

    feat: add user signIn and registration
    Add procedure for user signIn.
    Add procedure for user registration.

---
+++ src/server/trpc/router/user.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import { router, publicProcedure } from "../trpc";
+import argon2 from "argon2";
+import { TRPCError } from "@trpc/server";
+
+export const userRouter = router({
+  registerUser: publicProcedure
+    .input(z.object({ email: z.string().email(), password: z.string().min(8) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.user.create({
+        data: {
+          email: input.email,
+          password: await argon2.hash(input.password),
+        },
+      });
+    }),
+  signInUser: publicProcedure
+    .input(z.object({ email: z.string().email(), password: z.string().min(8) }))
+    .query(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findFirst({
+        where: {
+          email: input.email,
+        },
+      });
+
+      if (!user)
+        throw new TRPCError({
+          message: "User is not registered",
+          code: "NOT_FOUND",
+        });
+
+      const isPasswordCorrect = await argon2.verify(
+        user.password,
+        input.password
+      );
+
+      if (!isPasswordCorrect)
+        throw new TRPCError({
+          message: "Password is incorrect",
+          code: "UNAUTHORIZED",
+        });
+
+      return user;
+    }),
+});
